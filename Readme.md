cc3baa75e7a79358b26c315b5da66a7fec0e40e3
mah-fuz7 <mahafuz846@gmail.com>
2025-11-07 00:37:59 +0600

    ans the question

---
+++ Readme.md
@@ -0,0 +1,18 @@
+1. What is the difference between **getElementById, getElementsByClassName, and querySelector / querySelectorAll**?
+
+=>getElementById: when you select the element in js using id
+=>getElementByClassName:when you select the multiple element in js using class name
+=>querySelector:select the element using css selector and it only select the first element
+=>querySelectorAll: select the multiple element using css selector
+
+
+2. How do you **create and insert a new element into the DOM**?
+
+const newDiv=document.createElement("div");
+newDiv.innerHtml="<h1>Hellow World"</h1>
+const mainDiv=document.getElementById("main-div");
+mainDiv.append(newDiv)
+
+3. What is **Event Bubbling** and how does it work?
+4. What is **Event Delegation** in JavaScript? Why is it useful?
+5. What is the difference between **preventDefault() and stopPropagation()** methods?
--- script.js
+++ script.js
@@ -1,54 +1,48 @@
-
-
 // getelement function
-function getElement(id){
- return document.getElementById(id)
-  
+function getElement(id) {
+  return document.getElementById(id);
 }
 
+const cartButtons = document.getElementsByClassName("card-btn");
 
-const cartButtons=document.getElementsByClassName('card-btn');
+for (let cartButton of cartButtons) {
+  cartButton.addEventListener("click", function () {
+    let serviceName =
+      cartButton.parentNode.parentNode.children[1].children[0].innerText;
+    console.log(serviceName);
+    let serviceNumber =
+      cartButton.parentNode.parentNode.children[2].children[0].innerText;
+    console.log(serviceNumber);
 
-for(let cartButton of cartButtons){
-cartButton.addEventListener("click",function(){
+    //call button functionality
 
-let serviceName=cartButton.parentNode.parentNode.children[1].children[0].innerText
-console.log(serviceName)
-let serviceNumber=cartButton.parentNode.parentNode.children[2].children[0].innerText
-console.log(serviceNumber)
+    //  coin
+    let call_coin = getElement("coin").innerText;
 
+    console.log(call_coin.innerText);
 
-//call button functionality
+    //  call alert ,cut coin , insufficient Blance
 
-
-    //  coin 
-let call_coin=getElement('coin').innerText
-
-console.log(call_coin.innerText)
-
-//  call alert ,cut coin , insufficient Blance
-
-if(call_coin<=0){
-    alert(`❌Insufficient coins. Please top up to continue.`)
-}else{
-    alert(`
+    if (call_coin <= 0) {
+      alert(`❌Insufficient coins. Please top up to continue.`);
+    } else {
+      alert(`
     ${serviceName} 
     ${serviceNumber}
 
-    `)
-let after_call_coin=Number(call_coin)-20
-getElement('coin').innerText=after_call_coin;
-}
+    `);
+      let after_call_coin = Number(call_coin) - 20;
+      getElement("coin").innerText = after_call_coin;
+    }
 
-// history
+    // history
 
+    if (call_coin > 0) {
+      let now = new Date();
+      let history_container = getElement("history_box");
 
-if(call_coin>0){
-    let now = new Date();
-let history_container=getElement('history_box');
-
-let newcart=document.createElement('div');
-    newcart.innerHTML=`
+      let newcart = document.createElement("div");
+      newcart.innerHTML = `
 <div class="bg-gray-100 rounded-xl flex justify-between p-4 mx-2 my-3">
    
     <div class="">
@@ -58,51 +52,44 @@ let newcart=document.createElement('div');
     </div>
     <div class="pt-3">${now.toLocaleTimeString()} </div>
 </div>
-`
-history_container.appendChild(newcart)
-}else{
-
-}
-
-
-
-
-
-})
-
-
+`;
+      history_container.appendChild(newcart);
+    } else {
+    }
+  });
 }
 
 // clear button
-getElement('clear-btn').addEventListener('click',function(){
-    let clear_history=getElement('history_box')
-    clear_history.innerHTML=''
-})
+getElement("clear-btn").addEventListener("click", function () {
+  let clear_history = getElement("history_box");
+  clear_history.innerHTML = "";
+});
 
 // heart icon functionality
-let heartLogos=document.getElementsByClassName('heart_icon')
-    for(let heartLogo of heartLogos){
-        heartLogo.addEventListener('click',function(){
-            let heart_count=getElement('heart_count').innerText
-            console.log(heart_count)
-            let afterClick=Number(heart_count)+1
-            getElement('heart_count').innerText=afterClick
-        })
-    }
+let heartLogos = document.getElementsByClassName("heart_icon");
+for (let heartLogo of heartLogos) {
+  heartLogo.addEventListener("click", function () {
+    let heart_count = getElement("heart_count").innerText;
+    console.log(heart_count);
+    let afterClick = Number(heart_count) + 1;
+    getElement("heart_count").innerText = afterClick;
+  });
+}
 
 // copy button functionality
 
-let copyBtns=document.getElementsByClassName('copy-btn')
-    for(let copyBtn of copyBtns){
-        copyBtn.addEventListener('click',function(){
-            alert("Number copied")
-            let copy_count=getElement('copy_count').innerText
-            
-            let afterCopy=Number(copy_count)+1
-            getElement('copy_count').innerText=afterCopy
+let copyBtns = document.getElementsByClassName("copy-btn");
+for (let copyBtn of copyBtns) {
+  copyBtn.addEventListener("click", function () {
+    alert("Number copied");
+    let copy_count = getElement("copy_count").innerText;
 
-            let service_number=copyBtn.parentNode.parentNode.children[2].children[0].innerText
-            console.log(service_number)
-           navigator.clipboard.writeText(service_number)
-        })
-    }
+    let afterCopy = Number(copy_count) + 1;
+    getElement("copy_count").innerText = afterCopy;
+
+    let service_number =
+      copyBtn.parentNode.parentNode.children[2].children[0].innerText;
+    console.log(service_number);
+    navigator.clipboard.writeText(service_number);
+  });
+}
